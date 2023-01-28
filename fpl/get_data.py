55822750e2cdde31f0b8ecd2f2bce63260161f18
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2023-01-28 03:34:57 +0300

    added fpl api

--- fpl/get_data.py
+++ fpl/get_data.py
@@ -1,5 +1,4 @@
 import requests
-import json
 
 FPL_URL = "https://fantasy.premierleague.com/api/bootstrap-static/"
 
@@ -7,6 +6,5 @@ FPL_URL = "https://fantasy.premierleague.com/api/bootstrap-static/"
 def get_game_data():
     # get json object from fpl api
     data = requests.get(FPL_URL).json()
-    json_pretty = data
 
-    return json_pretty
+    return data
